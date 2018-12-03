8cffc209592b645cf91c58b0079ba7a7efce6871
Alejandro Bolivar <bolivaral@gmail.com>
2018-12-02 19:52:48 -0500

    Played with chapter 2 stuff

---
+++ variables.py
@@ -0,0 +1,12 @@
+name = "ada lovelace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# print("Hello, " + name.title() + "!")
+# print("Languages:\n\tPython\n\tC\n\tJavaScript")
+favorite_language = "python "
+print(favorite_language)
+print(favorite_language.rstrip())
+fav_lan = ' python'
+print(fav_lan)
+print(fav_lan.lstrip())
